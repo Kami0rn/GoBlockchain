3b09dce3266aec5b380ec1fea002b654a7f8d48a
Kami0rn <74646193+Kami0rn@users.noreply.github.com>
2025-04-13 18:13:47 +0700

    Refactor blockchain structure: move TxInput and TxOutput types to tx.go, implement CommandLine methods in cli.go, and create wallet package with Wallet struct.

--- project02/blockchain/transaction.go
+++ project02/blockchain/transaction.go
@@ -15,16 +15,6 @@ type Transaction struct {
 	Outputs []TxOutput
 }
 
-type TxOutput struct {
-	Value int
-	PubKey string 
-}
-
-type TxInput struct {
-	ID []byte
-	Out int
-	Sig string 
-}
 
 func (tx *Transaction) SetID() {
 	var encoded bytes.Buffer
@@ -89,10 +79,3 @@ func (tx *Transaction) IsCoinbase() bool {
 	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
 }
 
-func (in *TxInput) CanUnlock(data string) bool {
-	return in.Sig == data
-}
-
-func (out *TxOutput) CanBeUnlocked(data string) bool {
-	return out.PubKey == data
-}

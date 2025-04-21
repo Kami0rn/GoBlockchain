8987493d4eaa62e6252ad6fc92abbf19da98a9a1
Kami0rn <74646193+Kami0rn@users.noreply.github.com>
2025-04-21 17:24:00 +0700

    refactor: Improve code formatting and readability in transaction.go

--- project02/blockchain/transaction.go
+++ project02/blockchain/transaction.go
@@ -3,12 +3,14 @@ package blockchain
 import (
 	"bytes"
 	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/gob"
 	"encoding/hex"
 	"fmt"
 	"log"
+	"math/big"
 	"runtime/trace"
 
 	"golang.org/x/text/unicode/rangetable"
@@ -16,8 +18,8 @@ import (
 )
 
 type Transaction struct {
-	ID []byte
-	Inputs []TxInput
+	ID      []byte
+	Inputs  []TxInput
 	Outputs []TxOutput
 }
 
@@ -38,11 +40,10 @@ func (tx *Transaction) Hash() []byte {
 	txCopy := *tx
 	txCopy.ID = []byte{}
 
-	hash = sha256.Sum256(txCopy.Serialize())    
-	
+	hash = sha256.Sum256(txCopy.Serialize())
+
 	return hash[:]
 }
-
 
 func (tx *Transaction) SetID() {
 	var encoded bytes.Buffer
@@ -62,18 +63,18 @@ func CoinBaseTx(to, data string) *Transaction {
 	}
 
 	txin := TxInput{[]byte{}, -1, data}
-	txout := TxOutput{100,to}
+	txout := TxOutput{100, to}
 
-	tx := Transaction{nil,[]TxInput{txin},[]TxOutput{txout}}
+	tx := Transaction{nil, []TxInput{txin}, []TxOutput{txout}}
 	tx.SetID()
 
 	return &tx
 }
 
-func NewTransaction(from, to string, amount int, chain *BlockChain) *Transaction{
+func NewTransaction(from, to string, amount int, chain *BlockChain) *Transaction {
 	var inputs []TxInput
 	var outputs []TxOutput
-	
+
 	acc, validOutputs := chain.FindSpendableOutputs(from, amount)
 
 	if acc < amount {
@@ -84,7 +85,7 @@ func NewTransaction(from, to string, amount int, chain *BlockChain) *Transaction
 		txID, err := hex.DecodeString(txid)
 		Handle(err)
 
-		for _,out := range outs {
+		for _, out := range outs {
 			input := TxInput{txID, out, from}
 			inputs = append(inputs, input)
 		}
@@ -99,7 +100,6 @@ func NewTransaction(from, to string, amount int, chain *BlockChain) *Transaction
 	tx := Transaction{nil, inputs, outputs}
 	tx.SetID()
 
-
 	return &tx
 }
 
@@ -107,8 +107,8 @@ func (tx *Transaction) IsCoinbase() bool {
 	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
 }
 
-func (tx *Transaction) Sign(PrivKey ecdsa.PrivateKey, prevTXs map[string]Transaction){
-	if tx.IsCoinbase(){
+func (tx *Transaction) Sign(PrivKey ecdsa.PrivateKey, prevTXs map[string]Transaction) {
+	if tx.IsCoinbase() {
 		return
 	}
 
@@ -125,7 +125,7 @@ func (tx *Transaction) Sign(PrivKey ecdsa.PrivateKey, prevTXs map[string]Transac
 		txCopy.Inputs[inId].Signature = nil
 		txCopy.Inputs[inId].PubKey = prevTX.Outputs[in.Out].PubKeyHash
 		txCopy.ID = txCopy.Hash()
-		txCopy.Inputs[inId].PubKey = nil 
+		txCopy.Inputs[inId].PubKey = nil
 
 		r, s, err := ecdsa.Sign(rand.Reader, &PrivKey, tx.ID)
 		Handle(err)
@@ -158,6 +158,49 @@ func (tx *Transaction) Verify(prevTXs map[string]Transaction) bool {
 	}
 
 	for _, in := range tx.Inputs {
-		if
+		if prevTXs[hex.EncodeToString(in.ID)].ID == nil {
+			log.Panic("Previous transaction does not exist")
+		}
+	}
+
+	txCopy := tx.TrimmedCopy()
+	curve := elliptic.P256()
+
+	for inId, in := range tx.Inputs {
+		prevTx := prevTXs[hex.EncodeToString(in.ID)]
+		txCopy.Inputs[inId].Signature = nil
+		txCopy.Inputs[inId].PubKey = prevTx.Outputs[in.Out].PubKeyHash
+		txCopy.ID = txCopy.Hash()
+		txCopy.Inputs[inId].PubKey = nil
+
+		r := big.Int{}
+		s := big.Int{}
+		sigLen := len(in.Signature)
+		r.SetBytes(in.Signature[:(sigLen / 2)])
+		s.SetBytes(in.Signature[(sigLen / 2):])
+		
+		x := big.Int{}
+		y := big.Int{}
+		keyLen := len(in.PubKey)
+		
+		x.SetBytes(in.PubKey[:(keyLen / 2)])
+		y.SetBytes(in.PubKey[(keyLen / 2):])
+
+		rawPubKey := ecdsa.PrivateKey{curve, &x, &y}
+		if ecdsa.Verify(&rawPubKey, txCopy.ID, &r, &s) == false {
+			return false
+		}
+
+	}
+
+	return true
+}
+
+func (tx Transaction) String() string {
+	var lines []string
+
+	lines = append(lines, fmt.Sprintf("--- Transaction %x:", tx.ID))
+	for i, input := range tx.Inputs {
+		
 	}
 }
